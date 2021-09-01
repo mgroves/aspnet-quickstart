bff4ab247eb81b9c48723e3037ec8cebea768bc8
Jagadesh Munta <jagadesh.munta@couchbase.com>
2021-08-31 18:29:54 -0700

    Fixing the paths on gitpod

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd ~/
-sudo wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo wget -q https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
 sudo apt-get update
@@ -8,9 +7,9 @@ sudo apt-get install -y sudo apt-transport-https
 sudo apt-get install -y dotnet-sdk-5.0
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u Administrator:password localhost:8091/pools/default/buckets/)" != "200" ]]; do sleep 5; done
-cd ~/aspnet-quickstart/src/Org.Quickstart.API
+cd src/Org.Quickstart.API
 dotnet build
 dotnet run &
 sleep 10
-cd ~/aspnet-quickstart/src/Org.Quickstart.IntegrationTests
+cd ../Org.Quickstart.IntegrationTests
 dotnet test
