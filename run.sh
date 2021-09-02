9f60f8c2476bc238ef98914ca4a1679096980891
Jagadesh Munta <jagadesh.munta@couchbase.com>
2021-09-02 10:45:54 -0700

    Changed the dotnet build to restore for dotnet issue

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u Administrator:password localhost:8091/pools/default/buckets/)" != "200" ]]; do sleep 5; done
 cd src/Org.Quickstart.API
-dotnet build
+dotnet restore
 dotnet run &
 sleep 10
 cd ../Org.Quickstart.IntegrationTests
+dotnet restore
 dotnet test
